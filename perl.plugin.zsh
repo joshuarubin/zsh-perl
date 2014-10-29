256360f104ebac650435134e1455c740dc2a60bf
Joshua Rubin <jrubin@zvelo.com>
2014-10-29 11:54:10 -0600

    remove perlbrew
    Signed-off-by: Joshua Rubin <jrubin@zvelo.com>

--- perl.plugin.zsh
+++ perl.plugin.zsh
@@ -18,14 +18,3 @@ fi
 alias pl='perl'
 alias pld='perldoc'
 alias ple='perl -wlne'
-
-# Perlbrew
-alias plb='perlbrew'
-alias plba='perlbrew available'
-alias plbi='perlbrew install'
-alias plbl='perlbrew list'
-alias plbo='perlbrew off'
-alias plbO='perlbrew switch-off'
-alias plbs='perlbrew switch'
-alias plbu='perlbrew use'
-alias plbx='perlbrew uninstall'
