2466217029e35af3627ded396b1b2007cf7f6386
Joshua Rubin <jrubin@zvelo.com>
2015-10-13 22:34:03 -0600

    sync with prezto

--- perl.plugin.zsh
+++ perl.plugin.zsh
@@ -11,6 +11,27 @@ if (( ! $+commands[perl] )); then
 fi
 
 #
+# Local Module Installation
+#
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  # Perl is slow; cache its output.
+  cache_file="${0:h}/cache.zsh"
+  perl_path="$HOME/Library/Perl/5.18"
+
+  if [[ -f "/Library/Perl/5.18/local/lib.pm" ]]; then
+    if [[ ! -s "$cache_file" ]]; then
+      perl -I$perl_path/lib/perl5 -Mlocal::lib=$perl_path >! "$cache_file"
+    fi
+
+    source "$cache_file"
+  fi
+
+  unset perl_path
+  unset cache_file
+fi
+
+#
 # Aliases
 #
 
@@ -18,3 +39,14 @@ fi
 alias pl='perl'
 alias pld='perldoc'
 alias ple='perl -wlne'
+
+# Perlbrew
+alias plb='perlbrew'
+alias plba='perlbrew available'
+alias plbi='perlbrew install'
+alias plbl='perlbrew list'
+alias plbo='perlbrew off'
+alias plbO='perlbrew switch-off'
+alias plbs='perlbrew switch'
+alias plbu='perlbrew use'
+alias plbx='perlbrew uninstall'
